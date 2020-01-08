9898232a62e84b8d8a31242800524af1c82e3dec
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-01-08 13:41:26 +0545

    Added Interceptor code to springMvc project and added a trial of springboot based logging project

---
+++ slf4-spring-boot/src/main/java/com/javacodegeeks/App.java
@@ -0,0 +1,14 @@
+package com.javacodegeeks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("This is how you configure Java Logging with SLF4J");
+    }
+}
